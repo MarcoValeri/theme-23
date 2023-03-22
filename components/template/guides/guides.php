b1d96a95c06ab0bdab1c80fb4855d3c579a15ff4
MarcoValeri <marcovaleri@hotmail.it>
2023-03-22 15:34:03 +0000

    Add content component

---
+++ components/template/guides/guides.php
@@ -0,0 +1,30 @@
+<?php
+
+// http://bestpractices.local/wp-json/wp/v2/posts
+// http://bestpractices.local/wp-json/wp/v2/categories
+
+// $guidesArgs = [
+//     'post_type'         => 'post',
+//     'post_per_page'     => -1
+// ];
+
+// $guidesQuery = new WP_Query($guidesArgs);
+
+?>
+
+<section class="guides">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="guides__col col-2">
+                <div class="guides__container-sidebar">
+                    <h2>sidebar</h2>
+                </div>
+            </div>
+            <div class="guides__col col-10">
+                <div class="guides__container-content">
+                    <h2>content</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ components/template/guides/hooks.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace A\Components\Guides;
+
+\add_filter('a/components/guides', __NAMESPACE__ . '\\parseArgs');
